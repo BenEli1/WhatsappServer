f31a9ce0c5900d32f5a54d49b6cdba2bcaeeaaf1
Ben <benelashvlili@gmail.com>
2022-05-08 18:35:56 +0300

    Add project files.

---
+++ whatsappProject/Models/Message.cs
@@ -0,0 +1,15 @@
+﻿
+namespace whatsappProject.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+
+        public string Text { get; set; }
+
+        public string Date { get; set; }
+
+        public string InOut { get; set; }
+
+    }
+}
---
+++ whatsappProject/Models/User - Copy.cs
@@ -0,0 +1,15 @@
+﻿namespace whatsappProject.Models
+{
+    public class User
+    {
+        [Key]
+        public string UserName { get; set; }
+
+        public string NickName { get; set; }
+
+        public int Password { get; set; }
+
+        public string Image { get; set; }
+
+    }
+}
